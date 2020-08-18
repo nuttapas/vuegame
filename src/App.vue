adba337af14f578647fa2b33b1cc5b1befdd4972
nuttapas <68525325+nuttapas@users.noreply.github.com>
2020-08-18 13:04:29 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <vuegame/>
+  <p>{{choseName}}</p>
+</div>
+</template>
+
+<script>
+import vuegame from './components/vuegame.vue'
+
+export default {
+  name: 'App',
+  components: {
+  vuegame,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/vuegame.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <p>{{choseName}}</p>
+  <button @click="piker">Choose!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data : function(){
+        return{
+            player: [
+                {name: 'ironman', hp: 200, img:'img/ironman.png'},
+                {name: 'doremon', hp: 180, img:'img/doremon.png'},
+                {name: 'anakin', hp: 190, img:'img/anakin.png'}
+            ],
+            monster: [
+                {name: 'thanos', hp: 230, img:'img/thanos.png'},
+                {name: 'darthvader', hp: 220, img:'img/darthvader.png'},
+                {name: 'cell', hp: 210, img:'img/cell.png'},
+            ],
+            chosenName: ''
+    }
+},
+methods : {
+    randomdamage : function(min,max){
+      return Math.max(Math.floor(Math.random() * max) + 1,min)
+    },
+  
+    picker: function(){
+      var chosenNumber = Math.floor(Math.random() * this.player.length);
+      this.chosenName = this.player[chosenNumber];
+    },
+    randommonster : function(){
+      return Math.floor(Math.random() * this.monster.length)
+    },
+    hpplayer : function(){
+        return (Math.floor * this.randomplayer[1]) - this.randomdamage
+    },
+    hpmonster : function(){
+        return (Math.floor * this.randommonster[1]) - this.randomdamage
+    },
+    },
+    
+}
+</script>
+
+<style>
+    
+</style>
